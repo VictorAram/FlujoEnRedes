5c17b2803b86561705a1b4b07713ab5eff6e26fb
Victor Dominguez <33969502+VictorAram@users.noreply.github.com>
2018-04-22 10:44:46 -0500

    Update proy4.py

--- Tarea4/proy4.py
+++ Tarea4/proy4.py
@@ -8,7 +8,8 @@ class Grafo:
         self.vecinos=dict()
         self.aristas=dict()
         self.nodos=[]
-        #self.pos=dict()
+        self.V= dict()
+        self.E=dict()
 
     #nodos al rededor del centro
     def agrega(self, v, c, rad, ang):
@@ -16,6 +17,7 @@ class Grafo:
             for n in range(1, v+1):
                 x = rad*cos(ang*n) + c[0]
                 y = rad*sin(ang*n) + c[1]
+                self.V[v]=(x,y)
                 self.nodos.append((x,y))
                 print(x, y, file = crear)
                 if not (x, y) in self.vecinos:
@@ -27,11 +29,37 @@ class Grafo:
             for j in range(0,v):
                 a = self.nodos[j]
                 b = self.nodos[j-r]
+                #conección
+                self.E[(a, b)] = self.E[(b, a)]=r
                 self.aristas[(a,b)] = self.aristas[(b,a)] = r
                 self.vecinos[a].append(b)
                 self.vecinos[b].append(a)
 
-    def grafos(self):
+
+
+    def intermedio(self, h, m):
+        prob=0.1
+        p=self.nodos[h]
+        q=self.nodos[m]
+        self.aristas[(p,q)]=self.aristas[(q,p)]=h
+        self.vecinos[p].append(q)
+        self.vecinos[q].append(p)
+##    def intermedio(self):
+##        prob=0.001
+##        for (m1,n1) in self.nodos:
+##            for (m2,n2) in self.nodos:
+##                h=random()
+##                if h<prob and ((m1,n1),(m2,n2)) not in self.aristas and ((m1,n1),(m2,n2)) not in self.aristas:
+##                    alea=floor(i/2) #un nodo mayor que los ya seleccionados
+##                else:
+##                    p = self.nodos.index((m1,n1))
+##                    q = self.nodos.index((m2,n2))
+##                    self.aristas[(p,q)]=self.aristas[(q,p)]
+##                    self.vecinos[(m1,n1)].append((m2,n2))
+##                    self.vecinos[(m2,n2)].append((m1,n1))
+                    
+
+    def grafos(self ,h ,m ,v):
         with open("tareaT4.plot","w") as archivo:
             print("set term png", file=archivo)
             print("set output 'tarea4.png'", file=archivo)
@@ -46,22 +74,50 @@ class Grafo:
                 y1= key[0][1]
                 x2= key[1][0]
                 y2= key[1][1]
-                p = self.aristas[key]
                 print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
                 num+=1
+
+            run=0
+            for mor in self.aristas:
+                for fu in range(v):
+                    if fu==h:
+                        x1=mor[0][0]
+                        y1=mor[0][1]
+                        for tu in range(v):
+                            if tu==m:
+                                x2=mor[1][0]
+                                y2=mor[1][1]
+                                print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1".format(num,x1,y1,x2,y2),file=archivo)
+                run+=1
+
             print("show arrow", file=archivo)
             print("plot 'Nodos.dat' using 1:2 with points pt 7", file=archivo)
             print("quit()", file=archivo)
 
 v=10
-k=3
+mu=(v+1)//2
+k=2
+if k>mu:
+    k=k-1
+else:
+    k=k
+
+h=uniform(0,v)
+m=uniform(0,v)
+if h==m and h==(m-k) and h==(m+k):
+    m=uniform(0,v)
+else:
+    m=m
 c = (0.5, 0.5)
 rad = 0.4
 ang = (360/v)*(pi/180)
 G=Grafo()
 G.agrega(v, c, rad, ang)
 G.conecta(k)
-G.grafos()
+G.grafos(h,m,v)
+G.intermedio
+
+
 
 
       

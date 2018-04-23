d07284bffeb407820cf9794fac955ba1862caa8e
Victor Dominguez <33969502+VictorAram@users.noreply.github.com>
2018-04-23 01:25:20 -0500

    Update proy4.py

--- Tarea4/proy4.py
+++ Tarea4/proy4.py
@@ -6,11 +6,12 @@ class Grafo:
     def __init__(self):
         self.dis=dict()
         self.vecinos=dict()
-        self.aristas=dict()
+        self.aristas=[]
         self.nodos=[]
+        self.A=[]
         self.V= dict()
         self.E=dict()
-
+        
     #nodos al rededor del centro
     def agrega(self, v, c, rad, ang):
         with open("Nodos.dat", "w") as crear:
@@ -25,40 +26,90 @@ class Grafo:
 
     #conectaremos nodos correspondientes en k
     def conecta(self, k):
-        for r in range(1,k+1):
-            for j in range(0,v):
-                a = self.nodos[j]
-                b = self.nodos[j-r]
-                #conección
-                self.E[(a, b)] = self.E[(b, a)]=r
-                self.aristas[(a,b)] = self.aristas[(b,a)] = r
-                self.vecinos[a].append(b)
-                self.vecinos[b].append(a)
+        for j in range(0,v):
+            x1 = self.nodos[j][0]
+            y1 = self.nodos[j][1]
+            for r in range(1,k+1):
+                x2 = self.nodos[j-r][0]
+                y2 = self.nodos[j-r][1]
+                #conexión
+                #self.E[(a, b)] = self.E[(b, a)]=r
+                self.aristas.append((x1,y1,x2,y2))
+                #self.aristas[(a,b)] = self.aristas[(b,a)] = r
+                #self.vecinos[a].append(b)
+                #self.vecinos[b].append(a)
 
-
+    def Inter(self, h, m, v):
+        prob=random()
+        maxi=0.2
+        if prob > maxi:
+            for g in range(v):
+                t1=self.nodos[h][0]
+                n1=self.nodos[h][1]
+                t2=self.nodos[m][0]
+                n2=self.nodos[m][1]
+                self.aristas.append((t1,n1,t2,n2))
 
     def intermedio(self, h, m):
-        prob=0.1
-        p=self.nodos[h]
-        q=self.nodos[m]
-        self.aristas[(p,q)]=self.aristas[(q,p)]=h
-        self.vecinos[p].append(q)
-        self.vecinos[q].append(p)
-##    def intermedio(self):
-##        prob=0.001
-##        for (m1,n1) in self.nodos:
-##            for (m2,n2) in self.nodos:
-##                h=random()
-##                if h<prob and ((m1,n1),(m2,n2)) not in self.aristas and ((m1,n1),(m2,n2)) not in self.aristas:
-##                    alea=floor(i/2) #un nodo mayor que los ya seleccionados
-##                else:
-##                    p = self.nodos.index((m1,n1))
-##                    q = self.nodos.index((m2,n2))
-##                    self.aristas[(p,q)]=self.aristas[(q,p)]
-##                    self.vecinos[(m1,n1)].append((m2,n2))
-##                    self.vecinos[(m2,n2)].append((m1,n1))
-                    
+        prob=random()
+        maxi=0.2
+        if prob > maxi:
+            p=self.nodos[h]
+            q=self.nodos[m]
+            self.aristas[(p,q)]=self.aristas[(q,p)]=h
+            self.vecinos[p].append(q)
+            self.vecinos[q].append(p)
 
+    def floyd_warshall(self):
+        self.d = {}
+        for (x,y) in self.nodos:
+            self.d[((x,y),(x,y))] = 0 # distancia reflexiva es cero
+            for u in self.vecinos[(x,y)]: # para vecinos, la distancia es el peso
+                self.d[((x,y),u)] = self.aristas[((x,y),u)]
+        for intermedio in self.nodos:
+            for desde in self.nodos:
+                for hasta in self.nodos:
+                    di = None
+                    if (desde, intermedio) in self.d:
+                        di = self.d[(desde, intermedio)]
+                    ih = None
+                    if (intermedio, hasta) in self.d:
+                        ih = self.d[(intermedio, hasta)]
+                    if di is not None and ih is not None:
+                        c = di + ih # largo del camino via "i"
+                        if (desde, hasta) not in self.d or c < self.d[(desde, hasta)]:
+                            self.d[(desde, hasta)] = c # mejora al camino actual
+        return self.d
+    
+        def Coeficiente(self):
+        self.sumDis = 0
+        for u in self.d:
+            self.sumDis = self.sumDis + self.d[u]
+        self.argDis = self.sumDis/len(self.d)
+        self.densidad = []
+        for (x,y) in self.nodos:
+            self.lpq = []
+            for lpq2 in self.aristas:
+                self.lpq.append(lpq2)
+            numvecino = len(self.vecinos[(x,y)]) -1
+            for t in range(numvecino):
+                print("el vecino es")
+                
+                self.clustCoef = 0
+                h = self.vecinos[(x,y)][t]
+                print(h)
+                for m in self.vecinos[h]:
+                    
+                    if m in self.vecinos[(x,y)] and m is not (x,y) and m is not (y,x):
+                        print("el vecino del vecino es")
+                        print(m)
+                        if (h,m) in self.lpq:
+                            self.lpq.remove((h,m))
+                            self.clustCoef = self.clustCoef + 1
+                       
+            dens = 2*self.clustCoef/((numvecino)*(numvecino-1))
+            self.densidad.append(dens)
+            
     def grafos(self ,h ,m ,v):
         with open("tareaT4.plot","w") as archivo:
             print("set term png", file=archivo)
@@ -69,32 +120,19 @@ class Grafo:
             print("set size square", file=archivo)
             print("set key off", file=archivo)
             num=0
-            for key in self.aristas:
-                x1= key[0][0]
-                y1= key[0][1]
-                x2= key[1][0]
-                y2= key[1][1]
-                print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
+            for a in self.aristas:
+
+                (x1,y1,x2,y2)=a
+
+                (t1,n1,t2,n2)=a
+                print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
+                print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1".format(num+1,t1,n1,t2,n2),file=archivo)
                 num+=1
-
-            run=0
-            for mor in self.aristas:
-                for fu in range(v):
-                    if fu==h:
-                        x1=mor[0][0]
-                        y1=mor[0][1]
-                        for tu in range(v):
-                            if tu==m:
-                                x2=mor[1][0]
-                                y2=mor[1][1]
-                                print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1".format(num,x1,y1,x2,y2),file=archivo)
-                run+=1
-
             print("show arrow", file=archivo)
             print("plot 'Nodos.dat' using 1:2 with points pt 7", file=archivo)
             print("quit()", file=archivo)
 
-v=10
+v=15
 mu=(v+1)//2
 k=2
 if k>mu:
@@ -114,10 +152,10 @@ ang = (360/v)*(pi/180)
 G=Grafo()
 G.agrega(v, c, rad, ang)
 G.conecta(k)
+G.Inter
+G.floy_warshall
+G.Coeficiente
 G.grafos(h,m,v)
-G.intermedio
 
 
 
-
-      
